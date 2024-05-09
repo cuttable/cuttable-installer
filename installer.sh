8e90e70ee8823eee74f7261b605aafa2ce4f9a9e
Silla Tan <silla@cuttable.com>
2024-05-09 16:44:35 +0900

    update installer command

--- installer.sh
+++ installer.sh
@@ -13,5 +13,10 @@ mkdir -p "$CUTTA_DIR"
 
 echo "🛜 Downloading installer..."
 curl -s --create-dirs -O --output-dir "$CUTTA_DIR" $RUN_SCRIPT_URL
+echo "✅ Download"
 
 chmod +x "$CUTTA_DIR/run"
+
+echo "Run installer using the command" 
+# shellcheck disable=SC2088
+echo "~/.cutta/run"
