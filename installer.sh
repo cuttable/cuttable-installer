5024dd7cea894a00906f3b90df718b584fea9c80
Silla Tan <silla@cuttable.com>
2024-05-09 12:07:18 +0900

    update runner

--- installer.sh
+++ installer.sh
@@ -5,12 +5,7 @@ set -euo pipefail
 RUN_SCRIPT_URL="https://raw.githubusercontent.com/cuttable/cuttable-installer/create-installer/run"
 CUTTA_DIR="$HOME/.cutta"
 
-# Get the system's temporary directory on macOS
-temp_dir=$(mktemp -d)
-
 # Create installer folder
-INSTALLER="$temp_dir/$(uuidgen)"
-
 echo "🧹 Cleaning $CUTTA_DIR..."
 rm -rf "$CUTTA_DIR"
 
